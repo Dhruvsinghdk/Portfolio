d856d45e0c79ea194b5b683e895396f4272cf395
Dhruvsinghdk <re621006@gmail.com>
2025-04-04 16:35:31 +0530

    new

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const resumebtns = document.querySelectorAll('.resume-btn');
 
 resumebtns.forEach((btn,idx)=>{
     btn.addEventListener('click',()=>{
+
+        const resumedetails = document.querySelectorAll('.resume-detail');
+
         resumebtns.forEach(btn =>{
             btn.classList.remove('active')
         });
         btn.classList.add('active');
+
+        resumedetails.forEach(details =>{
+            details.classList.remove('active');
+        });
+        resumedetails[idx].classList.add('active')
     });
 });
 
