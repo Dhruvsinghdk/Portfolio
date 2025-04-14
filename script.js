b7972b540aa1a6a1322c74dc5b9c39c33c1fee39
Dhruvsinghdk <re621006@gmail.com>
2025-04-14 15:46:39 +0530

    new

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ resumebtns.forEach((btn,idx)=>{
 });
 
 
+const arrowright = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowleft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activeportfolio = ()=>{
+    const imgslide = document.querySelector('.portfolio-carousel .img-slide');
+
+    imgslide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+}
+
+arrowright.addEventListener('click', ()=>{
+    if(index < 4){
+        index++;
+        arrowleft.classList.remove('disabled')
+    }
+    else{
+        index = 5;
+        arrowright.classList.add('disabled');
+    }
+
+    activeportfolio();
+});
+
+arrowleft.addEventListener('click', ()=>{
+    if(index > 1){
+        index--;
+        arrowright.classList.remove('disabled')
+    }
+    else{
+        index = 0;
+        arrowleft.classList.add('disabled');
+    }
+
+    activeportfolio();
+});
